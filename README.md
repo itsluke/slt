55db9d4f3c1d75e948a3bafc37b8dc7242419c59
Luke Ngakane <luke@lukecreates.com>
2013-06-30 12:34:09 -0400

    Improved Readme, created ENV values, reset all sensitive information.

---
+++ README.md
@@ -0,0 +1,6 @@
+== Sur Le Trottoir
+
+For those times when you want to let people know about something cool on the sidewalk
+"It's twitter for the streets"
+
+made by luke@lukecreates.com
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Surletrottoir::Application.config.secret_token = 'bb96e96f91673451c2e33ca4ca5b0f309930bb5520d16386484e740919292ee6434ef0663733ee3414e29cdd954527c6bcbb2868a3d495a584cdfbbd50e32a16'
+Surletrottoir::Application.config.secret_token = ENV['SECRET_TOKEN']
